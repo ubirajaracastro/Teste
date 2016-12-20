cb4ff82693050a3518f59191c0a0cdd277d94468
ubirajara_castro@hotmail.com <Vacaloca171>
2016-12-20 17:03:43 -0200

    Codigo

---
+++ Algoritmos/Form1.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace Algoritmos
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtMatricula = new System.Windows.Forms.TextBox();
+            this.txtNota1 = new System.Windows.Forms.TextBox();
+            this.Nota1 = new System.Windows.Forms.Label();
+            this.txtNota2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtNota3 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.Calcular = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(39, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Matrícula";
+            // 
+            // txtMatricula
+            // 
+            this.txtMatricula.Location = new System.Drawing.Point(97, 31);
+            this.txtMatricula.Name = "txtMatricula";
+            this.txtMatricula.Size = new System.Drawing.Size(77, 20);
+            this.txtMatricula.TabIndex = 1;
+            // 
+            // txtNota1
+            // 
+            this.txtNota1.Location = new System.Drawing.Point(81, 67);
+            this.txtNota1.Name = "txtNota1";
+            this.txtNota1.Size = new System.Drawing.Size(64, 20);
+            this.txtNota1.TabIndex = 3;
+            this.txtNota1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // Nota1
+            // 
+            this.Nota1.AutoSize = true;
+            this.Nota1.CausesValidation = false;
+            this.Nota1.Location = new System.Drawing.Point(39, 74);
+            this.Nota1.Name = "Nota1";
+            this.Nota1.Size = new System.Drawing.Size(36, 13);
+            this.Nota1.TabIndex = 2;
+            this.Nota1.Text = "Nota1";
+            // 
+            // txtNota2
+            // 
+            this.txtNota2.Location = new System.Drawing.Point(232, 67);
+            this.txtNota2.Name = "txtNota2";
+            this.txtNota2.Size = new System.Drawing.Size(64, 20);
+            this.txtNota2.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.CausesValidation = false;
+            this.label2.Location = new System.Drawing.Point(190, 74);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(39, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Nota 2";
+            // 
+            // txtNota3
+            // 
+            this.txtNota3.Location = new System.Drawing.Point(366, 67);
+            this.txtNota3.Name = "txtNota3";
+            this.txtNota3.Size = new System.Drawing.Size(64, 20);
+            this.txtNota3.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.CausesValidation = false;
+            this.label3.Location = new System.Drawing.Point(324, 74);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(39, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Nota 3";
+            // 
+            // Calcular
+            // 
+            this.Calcular.Location = new System.Drawing.Point(178, 143);
+            this.Calcular.Name = "Calcular";
+            this.Calcular.Size = new System.Drawing.Size(83, 31);
+            this.Calcular.TabIndex = 8;
+            this.Calcular.Text = "Calcular";
+            this.Calcular.UseVisualStyleBackColor = true;
+            this.Calcular.Click += new System.EventHandler(this.Calcular_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(507, 245);
+            this.Controls.Add(this.Calcular);
+            this.Controls.Add(this.txtNota3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtNota2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtNota1);
+            this.Controls.Add(this.Nota1);
+            this.Controls.Add(this.txtMatricula);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Avaliação";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtMatricula;
+        private System.Windows.Forms.TextBox txtNota1;
+        private System.Windows.Forms.Label Nota1;
+        private System.Windows.Forms.TextBox txtNota2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtNota3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button Calcular;
+    }
+}
+
---
+++ Algoritmos/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Algoritmos
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Calcular_Click(object sender, EventArgs e)
+        {
+            string matricula = txtMatricula.Text;
+            double nota1 = double.Parse(txtNota1.Text);
+            double nota2 = double.Parse(txtNota2.Text);
+            double nota3 = double.Parse(txtNota3.Text);
+            string conceito = string.Empty;
+
+            bool APROVADO = false;
+
+
+            double media = (nota1 + (nota2*2) + (nota3*3))/6;
+
+
+            if (media >= 9)
+                conceito = "A";
+            else if (media >= 7.5 && media < 9)
+                conceito = "B";
+            else if (media >= 6 && media < 7.5)
+                conceito = "C";
+            else if (media >= 4 && media < 6)
+                conceito = "D";
+            else if (media < 4)
+                conceito = "D";
+
+            if (conceito == "A" || conceito == "B" || conceito == "C")
+                APROVADO = true;
+
+
+            string msg = "Matrícula: " + txtMatricula.Text;
+            msg += " Nota 1: " + txtNota1.Text;
+            msg += " Nota 2: " + txtNota2.Text;
+            msg += " Nota 3: " + txtNota3.Text;
+            msg += " Média: " + media.ToString();
+
+            if (APROVADO)
+                msg += " O Aluno está aprovado!";
+            else
+                msg += " O Aluno está reprovado.";
+
+
+
+            MessageBox.Show(msg, "Resultado da Avaliação:");
+
+
+
+        }
+    }
+}
