1280f4b9a4debd4231148e6ab2e6a6a65c970490
ubirajara_castro@hotmail.com <Vacaloca171>
2016-12-20 17:22:07 -0200

    Espaço

--- Algoritmos/Program.cs
+++ Algoritmos/Program.cs
@@ -17,6 +17,10 @@ namespace Algoritmos
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+
+
+
+
         }
     }
 }
